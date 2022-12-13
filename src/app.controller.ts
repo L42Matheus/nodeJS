f89a9e9690f32e12c9381a0f4e61af45186f1190
lucas.matheus <7vis3c2hskbf46dymabt76c3vtoqafuercjxu4dt77ru4k6tanaq>
2022-12-13 20:33:17 -0300

    add: database with connection prismaservice

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,29 @@
 import { Controller, Get } from '@nestjs/common';
+import { Post } from '@nestjs/common/decorators';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
+import { randomUUID } from 'crypto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  list() {
+    return this.prisma.notification.findMany();
   }
+
+  @Post()
+  async sendNotification() {
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content: "Você tem uma nova solicitação de amizade!",
+        category: "social",
+        recipientId: randomUUID()
+      }
+    })
+  }
+
+
 }
